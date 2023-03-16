b6bac43976be798c2a844f47452db489d9eb317d
Suhail <suhailha15@gmail.com>
2023-03-16 09:57:59 +0100

    Message

--- module_03/loops.py
+++ module_03/loops.py
@@ -1,5 +1,6 @@
 getal = int(input("voer een getal in "))
-print (f"de tafel van {getal} is:")
+
 for k in range(1,11):
     som = getal * k
     print (f"{k} x {getal} = {som}")
+print (f"de tafel van {getal} is:")
--- module_05/deel_1/calculator.py
+++ module_05/deel_1/calculator.py
@@ -14,7 +14,25 @@ def division(n1, n2):
 keuze = " "
 
 while not keuze in ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'):
-    
+
     keuzen = input("Wat wilt uw doen ? : a) getallen optellen : b) getallen afrtekken : c) getallen vermenigvuldigen d) getallen delen : e) getallen ophogen : f) getallen : g) getallen verdubbelen h) getalen halveren  ")
     if keuze == "i":
+        exit()
+
+aantal = -1
+while True:
+    if aantal > -1 and keuze != 'i' and keuze in ('a', 'b', 'c', 'd'):
+        n1 = aantal
+        n2 = int(input("Geef de 2e getal op: "))
+
+    elif aantal > -1 and keuze != 'i' and keuze in ('e', 'f', 'g', 'h'):
+        n1 = aantal
+ 
+    elif keuze in ('a', 'b', 'c', 'd'):
+     n1 = int(input("geef een getal op :"))
+     n2 = int(input("geef nog een getal op :"))
+
+    elif keuze in ('e', 'f', 'g', 'h'):
+        n1 = int(input("Geef een getal op: "))
+    else: 
         exit()
